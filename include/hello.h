e745973788957173120628fd84df1f18dd28948a
zhanglangdajiang <572620083@qq.com>
2022-11-04 16:39:54 +0800

    add hello.h

---
+++ include/hello.h
@@ -0,0 +1,16 @@
+//
+// Created by reol on 2022/11/4.
+//
+
+#ifndef REOP_HELLO_H
+#define REOP_HELLO_H
+
+#include<iostream>
+
+class hello {
+public:
+    hello() { cout << hello << "\n"; }
+};
+
+
+#endif //REOP_HELLO_H
